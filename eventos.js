2fdb4c18d0264aba916b31578cb1be9dc09e0069
Sebastian Barboza <sebabarboza74@gmail.com>
2023-08-13 22:28:21 -0300

    alerta en el div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+let divElement = document.getElementById('miDiv');
+
+// Agregar un manejador de eventos al hacer clic en el div
+divElement.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+    
+});
