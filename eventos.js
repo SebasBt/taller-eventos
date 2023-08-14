7e1d00fc5a5d04cb4ac57b4fec5db0a8e85b5e5c
Sebastian Barboza <sebabarboza74@gmail.com>
2023-08-13 22:46:10 -0300

    solucion a la doble alerta

--- eventos.js
+++ eventos.js
@@ -1,7 +1,9 @@
 let divElement = document.getElementById('miDiv');
 
-// Agregar un manejador de eventos al hacer clic en el div
+
 divElement.addEventListener('click', function() {
     alert('Hola! Soy el div');
+    
+    
     
 });
